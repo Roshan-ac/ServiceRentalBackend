8c2ab34baa765fec4645ad9565686b5fe11038f8
roshanac <roshanacharya3636@gmail.com>
2024-12-06 01:15:51 +0545

    fixed

---
+++ prisma/migrations/20241205172653_remove_uniquefield/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Post_customerId_key";
+
+-- DropIndex
+DROP INDEX "Session_customerId_key";
+
+-- DropIndex
+DROP INDEX "Session_freelancerId_key";
+
+-- DropIndex
+DROP INDEX "porposal_freelancerId_key";
